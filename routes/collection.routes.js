e1a4bb7b859381d2bca140b6c0c4f9daff8fb2eb
soorya2020 <sooryakrishnanunni@gmail.com>
2026-02-04 19:14:03 +0530

    ready for production v1

--- routes/collection.routes.js
+++ routes/collection.routes.js
@@ -11,13 +11,10 @@ import { authorize } from "../middleware/authorize.middleware.js";
 const collectionRouter = Router();
 
 collectionRouter.get("/collections", authorize, getCollections);
-
 collectionRouter.post("/collections", authorize, createCollection);
-
 collectionRouter.put("/collections/:id", authorize, updateCollectionTodos);
 collectionRouter.get("/collections/:id", authorize, getCollectionById);
 collectionRouter.delete("/collections/:id", authorize, deleteCollection);
 
-getCollectionById;
 
 export default collectionRouter;
